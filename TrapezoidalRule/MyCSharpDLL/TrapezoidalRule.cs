f47510922d4f7fb249f24c8153f287b2ee0c91e6
WikarNotAvailable <wikar8998@gmail.com>
2022-10-31 17:21:02 +0100

    setup of project

---
+++ TrapezoidalRule/MyCSharpDLL/TrapezoidalRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCSharpDLL
+{
+    public class TrapezoidalRule
+    {
+        public static int add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ TrapezoidalRule/TrapezoidalRule/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyCSharpDLL;
+
+namespace TrapezoidalRule
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+
+    public partial class MainWindow : Window
+    {
+        [DllImport(@"C:\Users\wikar\OneDrive\Pulpit\PROGRAMOWANIE\Trapezoidal-Rule\TrapezoidalRule\x64\Debug\MyAsm.dll")]
+        static extern int MyProc1(int a, int b);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
